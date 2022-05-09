0f4f5e7d723c7aa8ab069b4131b602cc6cc5df7a
Alex-Vis <usenkoshurik@mail.ru>
2022-05-09 12:37:06 +0300

    Initial commit

---
+++ src/main/java/com/alex/vis/soapwebsocket/SoapWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.alex.vis.soapwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alex/vis/soapwebsocket/SoapWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.vis.soapwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
